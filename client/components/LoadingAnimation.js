6675102eee63fa8711d155436dce82394afea9c2
Cem <cemaliakar114@hotmail.com>
2023-07-21 13:15:53 +0300

    Initial commit

--- client/components/LoadingAnimation.js
+++ client/components/LoadingAnimation.js
@@ -1,18 +1,18 @@
-import { View, Text, SafeAreaView } from "react-native";
-import * as Animatable from "react-native-animatable";
-import React from "react";
+import { View, Text, SafeAreaView } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import React from 'react';
 
 const LoadingAnimation = () => {
   return (
-    <View className="bg-[#EF233C] flex-1 ">
-      <SafeAreaView className="items-center m-auto">
+    <View className='bg-[#EF233C] flex-1 '>
+      <SafeAreaView className='items-center m-auto'>
         <Animatable.Text
-          className="text-white font-bold text-5xl mb-5"
-          animation="pulse"
-          easing="linear"
-          iterationCount="infinite"
+          className='text-white font-bold text-5xl mb-5'
+          animation='pulse'
+          easing='linear'
+          iterationCount='infinite'
         >
-          Comox's
+          Comox
         </Animatable.Text>
       </SafeAreaView>
     </View>
--- client/screens/EmptyBasketScreen.js
+++ client/screens/EmptyBasketScreen.js
@@ -1,22 +1,22 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
-import React from "react";
-import { useNavigation } from "@react-navigation/native";
-import XIconDelivery from "../utilities/XIconDelivery";
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import XIconDelivery from '../utilities/XIconDelivery';
 
 const EmptyBasketScreen = () => {
   const navigation = useNavigation();
 
   return (
-    <View className="bg-[#EF233C] flex-1 ">
+    <View className='bg-[#EF233C] flex-1 '>
       <TouchableOpacity
         onPress={navigation.goBack}
-        className="absolute top-4 left-4"
+        className='absolute top-4 left-4'
       >
         <XIconDelivery />
       </TouchableOpacity>
-      <SafeAreaView className="items-center m-auto">
-        <Text className="text-white font-bold text-4xl mb-5">Comox's </Text>
-        <Text className="text-white text-xl ">Your basket seems empty. 😓</Text>
+      <SafeAreaView className='items-center m-auto'>
+        <Text className='text-white font-bold text-4xl mb-5'>Comox </Text>
+        <Text className='text-white text-xl '>Your basket seems empty. 😓</Text>
       </SafeAreaView>
     </View>
   );
